f3104debf42e70c121a1e1d7711258acd614b3cd
hien4621 <hien4621@gmail.com>
2023-06-15 17:36:17 +0700

    7. Xây dựng màn contact

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ let homecontent = document.querySelector("#home");
 let resumecontent = document.querySelector("#resume");
 let portfoliocontent = document.querySelector("#portfolio");
 let blogcontent = document.querySelector("#blog");
+let contactcontent = document.querySelector("#contact");
 
 let hometab = document.querySelector(".hometab");
 let resumetab = document.querySelector(".resumetab");
 let portfoliotab = document.querySelector(".portfoliotab");
 let blogtab = document.querySelector(".blogtab");
+let contacttab = document.querySelector(".contacttab");
 
 hometab.addEventListener("click", () => {
     homecontent.classList.add("active");
     resumecontent.classList.remove("active");
     portfoliocontent.classList.remove("active");
     blogcontent.classList.remove("active");
+    contactcontent.classList.remove("active");
 }
 )
 resumetab.addEventListener("click", () => {
@@ -20,6 +23,7 @@ resumetab.addEventListener("click", () => {
     resumecontent.classList.add("active");
     portfoliocontent.classList.remove("active");
     blogcontent.classList.remove("active");
+    contactcontent.classList.remove("active");
 }
 )
 portfoliotab.addEventListener("click", () => {
@@ -27,6 +31,7 @@ portfoliotab.addEventListener("click", () => {
     resumecontent.classList.remove("active");
     portfoliocontent.classList.add("active");
     blogcontent.classList.remove("active");
+    contactcontent.classList.remove("active");
 }
 )
 blogtab.addEventListener("click", () => {
@@ -34,5 +39,14 @@ blogtab.addEventListener("click", () => {
     resumecontent.classList.remove("active");
     portfoliocontent.classList.remove("active");
     blogcontent.classList.add("active");
+    contactcontent.classList.remove("active");
+}
+)
+contacttab.addEventListener("click", () => {
+    homecontent.classList.remove("active");
+    resumecontent.classList.remove("active");
+    portfoliocontent.classList.remove("active");
+    blogcontent.classList.remove("active");
+    contactcontent.classList.add("active");
 }
 )
