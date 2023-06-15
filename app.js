e071084403b2ae0ee803993eb1ebd2dacd53e99b
hien4621 <hien4621@gmail.com>
2023-06-15 15:58:49 +0700

    6. Xây Dựng màn Blog

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
 let homecontent = document.querySelector("#home");
 let resumecontent = document.querySelector("#resume");
 let portfoliocontent = document.querySelector("#portfolio");
+let blogcontent = document.querySelector("#blog");
 
 let hometab = document.querySelector(".hometab");
 let resumetab = document.querySelector(".resumetab");
 let portfoliotab = document.querySelector(".portfoliotab");
+let blogtab = document.querySelector(".blogtab");
 
 hometab.addEventListener("click", () => {
     homecontent.classList.add("active");
     resumecontent.classList.remove("active");
     portfoliocontent.classList.remove("active");
+    blogcontent.classList.remove("active");
 }
 )
 resumetab.addEventListener("click", () => {
     homecontent.classList.remove("active");
     resumecontent.classList.add("active");
     portfoliocontent.classList.remove("active");
+    blogcontent.classList.remove("active");
 }
 )
 portfoliotab.addEventListener("click", () => {
     homecontent.classList.remove("active");
     resumecontent.classList.remove("active");
     portfoliocontent.classList.add("active");
+    blogcontent.classList.remove("active");
+}
+)
+blogtab.addEventListener("click", () => {
+    homecontent.classList.remove("active");
+    resumecontent.classList.remove("active");
+    portfoliocontent.classList.remove("active");
+    blogcontent.classList.add("active");
 }
 )
