ff79f4e94ac3ae3a19e20627b73beb9f82b4a79b
hien4621 <hien4621@gmail.com>
2023-06-15 14:38:44 +0700

    5.Xây dựng tab portfolio

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 let homecontent = document.querySelector("#home");
 let resumecontent = document.querySelector("#resume");
+let portfoliocontent = document.querySelector("#portfolio");
 
 let hometab = document.querySelector(".hometab");
 let resumetab = document.querySelector(".resumetab");
+let portfoliotab = document.querySelector(".portfoliotab");
 
 hometab.addEventListener("click", () => {
     homecontent.classList.add("active");
     resumecontent.classList.remove("active");
+    portfoliocontent.classList.remove("active");
 }
 )
 resumetab.addEventListener("click", () => {
     homecontent.classList.remove("active");
     resumecontent.classList.add("active");
+    portfoliocontent.classList.remove("active");
+}
+)
+portfoliotab.addEventListener("click", () => {
+    homecontent.classList.remove("active");
+    resumecontent.classList.remove("active");
+    portfoliocontent.classList.add("active");
 }
 )
